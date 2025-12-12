352b3fcd9d5819d3152d9891f28876434593252a
moula1008 <yama20061008@gmail.com>
2025-12-12 22:28:50 +0900

    add ymd

---
+++ react/src/components/goalsData/GoalData.jsx
@@ -0,0 +1,11 @@
+import styles from "./GoalData.module.css"
+
+export default function GoalData({goalData}){
+  return (
+    <div
+     className={styles.global}>
+      <p className={styles.date}>{goalData.date}まで</p>
+      <p className={styles.text}>{goalData.text}</p>
+    </div>
+  )
+}
---
+++ react/src/pages/AddPost/AddPost.jsx
@@ -0,0 +1,18 @@
+import ButtonBottomOption from "../../components/button/buttonBottomOption/ButtonBottomOption"
+
+export default function AddPost() {
+  return (
+    <>
+        <title>進捗の投稿</title>
+        <header className="header">
+            <h1>進捗の投稿</h1>
+        </header>
+        <main className="main">
+          ssss
+        </main>
+        <footer>
+            <ButtonBottomOption />
+        </footer>
+    </>
+  )
+}
