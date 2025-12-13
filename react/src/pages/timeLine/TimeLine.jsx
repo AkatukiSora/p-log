4fdda27cc0f83e59f4b84ed7552ea265203dbc22
MurakamiHonami <mnv2525@gmail.com>
2025-12-14 01:52:21 +0900

    API開発2

--- react/src/pages/timeLine/TimeLine.jsx
+++ react/src/pages/timeLine/TimeLine.jsx
@@ -1,5 +1,5 @@
 import ButtonBottomOption from "../../components//button/buttonBottomOption/ButtonBottomOption.jsx"
-import { useState,useEffect } from "react"
+import { useState,useEffect, use } from "react"
 import styles from "./TimeLine.module.css"
 import {BsEmojiNeutral} from "react-icons/bs"
 import { BsEmojiGrinFill } from "react-icons/bs";
@@ -53,40 +53,87 @@ export default function TimeLine() {
   //     icon: "./pig.png",
   //   },
   // }
-  const initialLikeCounts = {1:5,2:12,3:2,4:0};
-  const [likeCounts,setLikeCounts] = useState(initialLikeCounts);
 
-  const initialUserLikes = { 1: false, 2: true, 3: false,4:false};
-  const [userLiked, setUserLiked] = useState(initialUserLikes);
+  const [userLiked, setUserLiked] = useState({});
 
   const [posts,setPosts]=useState([]);
 
   useEffect(()=>{
     const fetchTimeLine=async()=>{
-      const res = await fetch(`${API_BASE_URL}/timeline`,{
+      const timelineRes = await fetch(`${API_BASE_URL}/timeline`,{
         headers: {
           'Authorization': `Bearer ${MOCK_TOKEN}`,
         },
       });
-      const data = await res.json();
+
+      const res = await timelineRes.json();
+      const userIds = res.map(item => item.user_id);
+
+      const userDataArray=await Promise.all(userIds.map(async (user_id)=>{
+        const userResponse = await fetch(`${API_BASE_URL}/users/${user_id}`,{
+          headers: {
+            'Authorization': `Bearer ${MOCK_TOKEN}`,
+          },
+        });
+        return userResponse.json();
+      }))
+
+      const iconArray=await Promise.all(userIds.map(async (user_id)=>{
+        const iconResponse=await fetch(`${API_BASE_URL}/users/${user_id}/icon`,{
+          headers: {
+            'Authorization': `Bearer ${MOCK_TOKEN}`,
+          },
+        });
+        return iconResponse;
+      }
+
+      ))
+
+      const goalIds=res.map(item => item.goal_id);
+      const titleArray = await Promise.all(goalIds.map(async (goalId)=>{
+        const postResponse = await fetch(`${API_BASE_URL}/goals/${goalId}`,{
+          headers: {
+            'Authorization': `Bearer ${MOCK_TOKEN}`,
+          },
+        });
+        return postResponse.json();
+
+      }))
+
+      const ids=res.map(item => item.id);
+      const postArray= await Promise.all(ids.map(async (id)=>{
+        const postReasponse = await fetch(`${API_BASE_URL}/posts/${id}`,{
+          headers: {
+            'Authorization': `Bearer ${MOCK_TOKEN}`,
+          },
+        });
+        return postReasponse.json();
+      }))
+      console.log(iconArray)
+      console.log(userDataArray)
+      console.log(titleArray)
+      console.log(res)
+      const data = res.map((item)=>({
+        ...item,
+        userData: userDataArray.find((data)=> data.id === item.user_id),
+        goalData: titleArray.find((data)=> data.id === item.goal_id),
+        postData: postArray.find((data)=> data.id === item.id),
+        icon: iconArray.find((data)=>data.user_id === item.user_id),
+      }));
       setPosts(data);
+      console.log(data)
     };
     fetchTimeLine();
 
-    const userIds = res.map
   },[]);
   const handleLikeToggle = (id) => {
-    const isCurrentlyLiked = userLiked[id];
 
-    setUserLiked(prev => ({
-      ...prev,
-      [id]: !isCurrentlyLiked,
-    }));
-
-    setLikeCounts(prevCounts => ({
-      ...prevCounts,
-      [id]: isCurrentlyLiked ? prevCounts[id] - 1 : prevCounts[id] + 1,
-    }));
+    const newLiked= {
+      id:id,
+      isReacted:!isReacted,
+      count: isReacted ? count - 1 : count + 1,
+    }
+    setUserLiked(newLiked)
   };
     // タイムライン
   return (
@@ -98,23 +145,22 @@ export default function TimeLine() {
       <main className="main">
         <div className={styles.default}>
           {posts.map((post)=>{
-            const user = users[post.userId];
-            const Liked = userLiked[post.id]
+            const reaction = 
             return(
               <div key={post.id} className={styles.post}>
                   <div>
-                    <img src={user.icon} alt={`${user.name}のアイコン`} className={styles.usericon}/>
-                    <strong>{user.name}</strong>
+                    <img src={post.icon} alt={`${post.userData.name}のアイコン`} className={styles.usericon}/>
+                    <strong>{post.userData.name}</strong>
                   </div>
                   <div>
-                    <div className={styles.option}>{post.title}</div>
+                    <div className={styles.option}>{post.goalData.title}</div>
                   <div className={styles.post}>
-                    <div className={styles.p}>{post.content}<small>({new Date(post.date).toLocaleDateString()})</small></div>
+                    <div className={styles.p}>{post.content}<small>({new Date(post.created_at).toLocaleDateString()})</small></div>
                     <button
                       onClick={() => handleLikeToggle(post.id)}
                       className={styles.button}
                     >
-                      {Liked ? <BsEmojiGrinFill/> : <BsEmojiNeutral />}{likeCounts[post.id]}
+                      {reaction.isReacted ? <BsEmojiGrinFill/> : <BsEmojiNeutral />}{post.postData.reaction_count}
                     </button>
                   </div>
                   </div>
