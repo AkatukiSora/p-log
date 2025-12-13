b8fcaa5a541f3da61a0c60c403aaa628f002dc3b
MurakamiHonami <mnv2525@gmail.com>
2025-12-13 14:46:27 +0900

    murakami

--- react/src/components/button/buttonTopMyOption/ButtonTopMyOption.jsx
+++ react/src/components/button/buttonTopMyOption/ButtonTopMyOption.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from "react-router-dom"
 import styles from "./ButtonTopMyOption.module.css"
 import { ROUTES } from "../../../pages/pathData/path"
@@ -21,27 +20,3 @@ export default function ButtonTopMyOption() {
     </>
   )
 }
-=======
-import { Link } from "react-router-dom"
-import styles from "./ButtonTopMyOption.module.css"
-import { ROUTES } from "../../../pages/pathData/path"
-
-export default function ButtonTopMyOption() {
-  return (
-    <>
-      <div className={styles.options}>
-        <Link className={styles.link} to={ROUTES.settingGoal}>
-            <div className={styles.option}>
-                <p>目標の追加</p>
-            </div>
-        </Link>
-        <Link className={styles.link} to={ROUTES.addPost}>
-            <div className={styles.option}>
-                <p>進捗の投稿</p>
-            </div>
-        </Link>
-      </div>
-    </>
-  )
-}
->>>>>>> front-develop
